483209c56e17e5e1c97dcc068ab589bce1f487f7
TheManishDaksh <140195332+TheManishDaksh@users.noreply.github.com>
2024-12-24 11:15:26 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 
-import React from 'react'
+import Header from "./sections/Header.jsx";
+import Hero from "./sections/Hero.jsx";
+import Features from "./sections/Features.jsx";
+import Pricing from "./sections/Pricing.jsx";
+import Faq from "./sections/Faq.jsx";
+import Testimonials from "./sections/Testimonials.jsx";
+import Download from "./sections/Download.jsx";
+import Footer from "./sections/Footer.jsx";
 
-function App() {
+const App = () => {
   return (
-    <div> 
-      <h1>Main App file </h1>
-    </div>
-  )
-}
+    <main className="overflow-hidden">
+      <Header />
+      <Hero />
+      <Features />
+      <Pricing />
+      <Faq />
+      <Testimonials />
+      <Download />
+      <Footer />
+    </main>
+  );
+};
 
-export default App
+export default App;
